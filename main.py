2b701838af2bddb75b7172409a4102265a89fc19
yagnesh <dave.1@iitj.ac.in>
2024-07-10 22:10:10 +0530

    Data Ingestion pipeline

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from chicken_disease_classifier import logger
+# logger.info("Welcome to my custom log")
+from chicken_disease_classifier.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to my custom log")
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
