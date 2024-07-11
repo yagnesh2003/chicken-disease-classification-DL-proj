06e586cd3bf83e6d7399072290c31ff1d658b054
yagnesh <dave.1@iitj.ac.in>
2024-07-11 17:37:13 +0530

    preparing base model

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from chicken_disease_classifier import logger
 # logger.info("Welcome to my custom log")
 from chicken_disease_classifier.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
+from chicken_disease_classifier.pipeline.stage02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -12,3 +13,14 @@ except Exception as e:
         logger.exception(e)
         raise e
 
+STAGE_NAME = "Prepare Base Model"
+
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+   obj = PrepareBaseModelTrainingPipeline()
+   obj.main()
+   logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+   logger.exception(e)
+   raise e
